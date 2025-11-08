febc2b624f37b4738aead5fcd774d0aa850eba5b
soliyajon <@soliyajonbekturdiyeva83>
2025-11-08 15:58:03 +0500

    done

--- index.js
+++ index.js
@@ -8,10 +8,106 @@ const TOKEN = "8347274799:AAE6lJwAk1g0j12GG0BFmLsfA9yMKI1OEbM"
 const bot =new TelegramBot(TOKEN , {polling:true});
  
 
-bot.on("message" , function(msg){
-    const chatId= msg.chat.id;
-    const text = msg.text;
 
 
-    bot.sendMessage(chatId , `Assalomu aleykum ==> ` )
-})
+bot.on("message", async function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+  const menuPhoto = "./rasm.jpg";
+  const drinks = "./suv.jpg";
+  const shirinlik = "./shirinlik.jpg";
+  const shashlik = "./shashlik.jpg";
+  const hotdog = "./hot dog.jpg";
+
+  if (text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 😜" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  } else if (text == "Boshlash 🔥") {
+    bot.sendPhoto(chatId, "./m5.jpg", {
+      caption: `Mercedes-Benz G-Class (Gelik) — bu dunyodagi eng mashhur va hashamatli yo‘ltanlamas avtomobillardan biridir. U 1979-yilda ishlab chiqarila boshlangan va shu kungacha o‘zining kuchi, ishonchliligi va o‘ziga xos dizayni bilan mashhur bo‘lib kelmoqda. “Gelik” nomi aslida “Geländewagen” so‘zining qisqartmasi bo‘lib, bu nemis tilida “yo‘ltanlamas avtomobil” degan ma’noni anglatadi.
+
+
+G63 AMG — Gelikning eng mashhur versiyalaridan biri bo‘lib, unda 4.0 litrli V8 biturbo dvigatel o‘rnatilgan. U 577 ot kuchi ishlab chiqaradi va 0 dan 100 km/soatgacha atigi 4.5 soniyada tezlashadi. Mashina 9 pog‘onali avtomatik uzatmalar qutisi va 4MATIC to‘liq g‘ildirakli tizimiga ega. Bu unga tog‘li yo‘llar, qumli hududlar yoki qorli joylarda bemalol yurish imkonini beradi.
+ Uning narxi modeliga qarab 150 000 dan 300 000 dollargacha bo‘lishi mumkin. G-Class bugun ham eng orzu qilingan mashinalardan biri bo‘lib qolmoqda.`,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "Info", callback_data: "info" },
+            { text: "photo", callback_data: "photo" },
+          ],
+          [
+            { text: "Price", callback_data: "price" },
+          ]
+        ]
+      }
+    })
+  } else if (text == "Menu 😜") {
+    const kutingXabari = await bot.sendMessage(chatId, "Iltimos kuting...");
+
+
+    setTimeout(function () {
+      bot.deleteMessage(chatId, kutingXabari.message_id);
+      bot.sendPhoto(chatId, menuPhoto, {
+        caption: "Bizning menyuyimiz...",
+        reply_markup: {
+          keyboard: [
+            [{ text: "Ichimlik" }, { text: "Shirinlik" }],
+            [{ text: "Shashlik" }, { text: "Hotdog" }],
+          ],
+        },
+      });
+    }, 1000);
+  }
+  bot.on("callback_query", function (query) {
+  console.log(query);
+  const data = query.data;
+  const chatId = query.message.chat.id;
+
+  if (data == "info") {
+    bot.sendMessage(chatId, "Bu yerda Lamborghini haqida ma'lumot olasiz");
+  } else if (data == "photos") {
+    bot.sendPhoto(chatId, "./images.jpg");
+  } else if (data == "price") {
+    bot.sendMessage(chatId, "175,000 dollar", {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Sotib olish", callback_data: "buy" }]],
+      },
+    });
+  } else if (data == "buy") {
+    bot.sendMessage(chatId, "Pullarni Soliyajonga bering... Mashina unda");
+  }
+});
+
+if (text== "Ichimlik") {
+   bot.sendPhoto(chatId, drinks, {
+    caption: `Bu yerda xohlagan ichimliklaringizni buyurtma qilishingiz mumkin.`
+  });
+}else if (text== "Shirinlik") {
+   bot.sendPhoto(chatId, shirinlik, {
+    caption: `Bu yerda xohlagan shirinliklaringizni buyurtma qilishingiz mumkin.`
+  });
+}else if (text== "Shashlik") {
+   bot.sendPhoto(chatId, shashlik, {
+    caption: `Bu yerda xohlagan turdagi shashliklaringizni buyurtma qilishingiz mumkin.`
+  });
+}else if (text== "Hotdog") {
+   bot.sendPhoto(chatId, hotdog, {
+    caption: `Bu yerda xohlagan turdagi hot doglaringizni buyurtma qilishingiz mumkin.`
+  });
+}
+
+
+  console.log(msg);
+  //   console.log("*********");
+  //   console.log(chatId, text);
+});
+
+console.log("Bot ishga tushdi...")
